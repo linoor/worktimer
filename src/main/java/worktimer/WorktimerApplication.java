f2559afde83d6f03ca1b6a9b46109924f59850a4
linoor <michal.pomaranski@gmail.com>
2016-09-05 21:05:33 +0200

    initial commit

---
+++ src/main/java/worktimer/WorktimerApplication.java
@@ -0,0 +1,12 @@
+package worktimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktimerApplication.class, args);
+	}
+}
