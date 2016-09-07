d45501db06b3ee1d2a82104b3c6862709da9a1a6
linoor <michal.pomaranski@gmail.com>
2016-09-07 21:57:34 +0200

    created commute class

--- src/main/java/worktimer/Commute.java
+++ src/main/java/worktimer/Commute.java
@@ -2,9 +2,8 @@ package worktimer;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by linoor on 9/5/16.
@@ -16,7 +15,21 @@ public class Commute {
     // time_started, time_ended, type, from_place, to_place, note
 
     private @Id @GeneratedValue Long id;
+
     private String note;
+    private String type;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endTime;
 
+    private Commute() {}
+
+    public Commute(String note, String type, Date startTime, Date endTime) {
+        this.note = note;
+        this.type = type;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 }
---
+++ src/main/java/worktimer/CommuteRepository.java
@@ -0,0 +1,9 @@
+package worktimer;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by linoor on 9/7/16.
+ */
+public interface CommuteRepository extends CrudRepository<Commute, Long> {
+}
