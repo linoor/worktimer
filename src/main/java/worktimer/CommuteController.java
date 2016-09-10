31d8c2d1da952683b1a8b1d1132004b1859d1dfe
linoor <michal.pomaranski@gmail.com>
2016-09-10 11:43:59 +0200

    starting and stopping commutes

--- src/main/java/worktimer/CommuteController.java
+++ src/main/java/worktimer/CommuteController.java
@@ -1,15 +1,24 @@
 package worktimer;
 
+import javaslang.control.Option;
+import javaslang.control.Try;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Resource;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by linoor on 9/8/16.
@@ -38,6 +47,31 @@ public class CommuteController {
         resource.add(entityLinks.linkToSingleResource(Commute.class, commute.getId()));
 
         return ResponseEntity.ok(resource);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/commutes/stop/{id}")
+    public @ResponseBody ResponseEntity<?> endCommute(@PathVariable("id") long id) {
+        try {
+            Commute commute = commuteRepository.findOne(id);
+
+            List<Measurement> stoppedMeasurements = measurementRepository.findAllByCommuteAndType(commute, Measurement.Type.STOP);
+            List<Measurement> startMeasurements = measurementRepository.findAllByCommuteAndType(commute, Measurement.Type.START);
+            if (startMeasurements.size() == 0) {
+                return ResponseEntity.status(HttpStatus.OK).body("This commute has not even been started");
+            }
+            else if (stoppedMeasurements.size() >= startMeasurements.size()) {
+                return ResponseEntity.status(HttpStatus.OK).body("This commute has already been stopped");
+            }
+
+            Measurement measurement = measurementRepository.save(new Measurement(Measurement.Type.STOP, commute));
+
+            Resource<Measurement> resource = new Resource<>(measurement);
+            resource.add(entityLinks.linkToSingleResource(Commute.class, commute.getId()));
+
+            return ResponseEntity.ok(resource);
+        } catch (NullPointerException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
--- src/main/java/worktimer/MeasurementRepository.java
+++ src/main/java/worktimer/MeasurementRepository.java
@@ -2,9 +2,12 @@ package worktimer;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by linoor on 9/8/16.
  */
 public interface MeasurementRepository extends CrudRepository<Measurement, Long> {
+    List<Measurement> findAllByCommuteAndType(Commute commute, Measurement.Type type);
 }
 
