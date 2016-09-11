d320665029d74e0d8a0e1a673db728afd8ab3731
linoor <michal.pomaranski@gmail.com>
2016-09-11 11:49:40 +0200

    created UI controller

--- src/main/java/worktimer/HomeController.java
+++ src/main/java/worktimer/HomeController.java
@@ -1,7 +1,17 @@
 package worktimer;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by linoor on 9/11/16.
  */
+
+@Controller
 public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
 }
