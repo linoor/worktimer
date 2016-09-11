00b3d0024a7b97f6fd4c36b53d63447655d479b0
linoor <michal.pomaranski@gmail.com>
2016-09-11 14:06:21 +0200

    added timer component

---
+++ src/main/resources/app/src/Timer.js
@@ -0,0 +1,27 @@
+/**
+ * Created by linoor on 9/11/16.
+ */
+
+import React, { Component } from 'react';
+import { Row } from "react-bootstrap";
+
+function str_pad_left(string,pad,length) {
+    return (new Array(length+1).join(pad)+string).slice(-length);
+}
+
+class Timer extends Component {
+    render() {
+        let minutes = str_pad_left(Math.floor(this.props.seconds / 60), '0', 2);
+        let seconds = str_pad_left(this.props.seconds - minutes * 60, '0', 2);
+
+        return (
+            <Row className="timer">
+                <span className="minutes">{minutes}</span>
+                <span>:</span>
+                <span className="seconds">{seconds}</span>
+            </Row>
+        )
+    }
+}
+
+export default Timer;
