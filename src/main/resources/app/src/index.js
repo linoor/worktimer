656ea1782c664f71d14c1dbeb8e983e520dbc25a
linoor <michal.pomaranski@gmail.com>
2016-09-25 12:33:52 +0200

    added react router

--- src/main/resources/app/src/index.js
+++ src/main/resources/app/src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// using an ES6 transpiler, like babel
+import { Router, Route, Link, browserHistory} from 'react-router'
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+        </Route>
+    </Router>
+), document.getElementById('root'));
+
