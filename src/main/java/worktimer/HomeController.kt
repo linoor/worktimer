66409a4696a1c79c787589b17e746c7b2876a8aa
linoor <michal.pomaranski@gmail.com>
2016-11-26 11:44:17 +0100

    changed one controller to Kotlin; fixed redirection problem

---
+++ src/main/java/worktimer/HomeController.kt
@@ -0,0 +1,25 @@
+package worktimer
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+/**
+ * Created by linoor on 9/11/16.
+ */
+
+@Controller
+@CrossOrigin(origins = arrayOf("http://localhost:3000"))
+class HomeController {
+
+    @RequestMapping(value = "/", method = arrayOf(RequestMethod.GET))
+    fun index(): String {
+        return "index"
+    }
+
+    @RequestMapping(value = "/commutes/*", method = arrayOf(RequestMethod.GET))
+    fun commutes(): String {
+        return "index"
+    }
+}
